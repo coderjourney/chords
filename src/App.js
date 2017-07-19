6111b6cfca1dd0d33e13cb8c851d992f03848f7c
Keith Thompson <me@keiththomps.com>
2017-07-19 08:59:41 -0400

    Add Blueprint.js and style application

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import ChordEditor from './components/ChordEditor';
+import SongList from './components/SongList';
 import { base } from './base';
 
 class App extends Component {
@@ -47,33 +48,26 @@ class App extends Component {
 
   render() {
     return (
-      <div className="wrapper">
-        <Header />
+      <div style={{maxWidth: "1160px", margin: "0 auto"}}>
         <BrowserRouter>
-          <div className="main-content">
-            <div className="workspace">
-              <Route exact path="/songs" render={(props) => {
-                const songIds = Object.keys(this.state.songs);
-                return (
-                  <ul>
-                    {songIds.map((id) => {
-                      return (
-                        <li key={id}>
-                          <Link to={`/songs/${id}`}>Song {id}</Link>
-                        </li>
-                      )
-                    })}
-                  </ul>
-                )
-              }} />
-              <Route path="/songs/:songId" render={(props) => {
-                const song = this.state.songs[props.match.params.songId];
-                return (
-                  song
-                  ? <ChordEditor song={song} updateSong={this.updateSong} />
-                  : <h1>Song not found</h1>
-                )
-              }} />
+          <div>
+            <Header />
+            <div className="main-content" style={{padding: "1em"}}>
+              <div className="workspace">
+                <Route exact path="/songs" render={(props) => {
+                  return (
+                    <SongList songs={this.state.songs} />
+                  )
+                }} />
+                <Route path="/songs/:songId" render={(props) => {
+                  const song = this.state.songs[props.match.params.songId];
+                  return (
+                    song
+                    ? <ChordEditor song={song} updateSong={this.updateSong} />
+                    : <h1>Song not found</h1>
+                  )
+                }} />
+              </div>
             </div>
           </div>
         </BrowserRouter>
--- src/components/ChordEditor.js
+++ src/components/ChordEditor.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Breadcrumb } from '@blueprintjs/core';
 import ChordSheetJS from 'chordsheetjs';
 
 class ChordEditor extends Component {
@@ -24,20 +25,27 @@ class ChordEditor extends Component {
 
   render() {
     return (
-      <div className="chord-editor">
-        <div className="panel">
-          <h3>Input</h3>
-          <textarea
-            style={{width: "100%", height: "100%"}}
-            onChange={this.handleChange}
-            value={this.props.song.chordpro}/>
-        </div>
-        <div className="panel">
-          <h3>Output</h3>
-          <div
-            style={{width: "100%", height: "100%", fontFamily: "monospace"}}
-            className="chord-output"
-            dangerouslySetInnerHTML={this.getChordMarkup()}/>
+      <div>
+        <ul className="pt-breadcrumbs">
+          <li><Breadcrumb href="/songs" text="Songs" /></li>
+          <li><Breadcrumb href="#" text={this.props.song.title} /></li>
+        </ul>
+        <h2 style={{margin: "0.5em 0"}}>{this.props.song.title}</h2>
+        <div className="chord-editor">
+          <div className="panel">
+            <h3>Input</h3>
+            <textarea
+              style={{width: "100%", height: "100%"}}
+              onChange={this.handleChange}
+              value={this.props.song.chordpro}/>
+          </div>
+          <div className="panel">
+            <h3>Output</h3>
+            <div
+              style={{width: "100%", height: "100%", fontFamily: "monospace"}}
+              className="chord-output"
+              dangerouslySetInnerHTML={this.getChordMarkup()}/>
+          </div>
         </div>
       </div>
     );
